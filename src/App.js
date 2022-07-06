87fa2f3ab7863f2a08386b506fd8fca3a3c8c397
lima-anderson <msgli34@gmail.com>
2022-07-06 20:12:38 -0300

    Adicionando e testando a biblioteca do Primereact no projeto

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ import 'toastr/build/toastr.css'
 import Rotas from './rotas';
 import NavBar from './components/navbar';
 
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+
+import { Button } from 'primereact/button';
+ 
+
 class App extends React.Component {
   render(){
     return (
       <div className="App">
+        <Button label="Click" icon="pi pi-check" />
         <NavBar />
         <Rotas/>
       </div>
